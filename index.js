54f5dd12713b243f85d60f463179ad02d5077bc9
Edvins Antonovs <ummahusla@gmail.com>
2017-09-24 14:44:08 +0100

    Added a menu

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const electron = require("electron");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on("ready", () => {
 	mainWindow = new BrowserWindow({});
 	mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+	const mainMenu = Menu.buildFromTemplate(menuTemplate);
+	Menu.setApplicationMenu(mainMenu);
 });
+
+const menuTemplate = [
+	{
+		label: "File",
+		submenu: [
+			{
+				label: "New todo"
+			}
+		]
+	}
+];
