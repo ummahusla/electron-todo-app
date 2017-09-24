c310a3f7b5d708866aaf998cca871b7ec0adaed4
Edvins Antonovs <ummahusla@gmail.com>
2017-09-24 17:04:02 +0100

    Created a new popup window

--- index.js
+++ index.js
@@ -3,24 +3,40 @@ const electron = require("electron");
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on("ready", () => {
 	mainWindow = new BrowserWindow({});
 	mainWindow.loadURL(`file://${__dirname}/main.html`);
+	mainWindow.on("closed", () => app.quit());
 
 	const mainMenu = Menu.buildFromTemplate(menuTemplate);
 	Menu.setApplicationMenu(mainMenu);
 });
+
+function createAddWindow() {
+	addWindow = new BrowserWindow({
+		width: 300,
+		height: 200,
+		title: "Add new todo"
+	});
+	addWindow.loadURL(`file://${__dirname}/add.html`);
+}
 
 const menuTemplate = [
 	{
 		label: "File",
 		submenu: [
 			{
-				label: "New todo"
+				label: "New todo",
+				accelerator: process.platform === "darwin" ? "Command+N" : "Ctrl+N",
+				click() {
+					createAddWindow();
+				}
 			},
 			{
 				label: "Quit",
+				accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
 				click() {
 					app.quit();
 				}
