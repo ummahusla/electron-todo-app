b11c763112c15c49729b4ceed1e0e68b3c6fe661
Edvins Antonovs <ummahusla@gmail.com>
2017-09-24 20:59:56 +0100

    Added a reload option

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ if (process.env.NODE_ENV !== "production") {
 	menuTemplate.push({
 		label: "View",
 		submenu: [
+			{ role: "reload" },
 			{
 				label: "Toggle Developer Tools",
 				accelerator: process.platform === "darwin" ? "Command+D" : "Ctrl+D",
